1d624856ee47360e8b33640d80d8f472dbb5ce49
dvasi <dvasi@gl34.student.cs.uwaterloo.ca>
2018-02-15 11:40:26 -0500

    made changes so puppet fuinisdhed except fix head rotation

--- Assets/puppet2.lua
+++ Assets/puppet2.lua
@@ -137,19 +137,150 @@ rightFoot:add_child(rightFootScale)
 rightFootScale:scale(0.05, 0.20, 0.05)
 rightFootScale:set_material(green) 
 
-neckJointMove = gr.node('neckJointMove')
-shoulder:add_child(neckJointMove)
-neckJointMove:translate(0.0, 0.43, 0.0)
+leftUpperArmJointMove = gr.node('leftUpperArmJointMove')
+shoulder:add_child(leftUpperArmJointMove)
+leftUpperArmJointMove :translate(0.55, -0.03, 0.0)
 
-neckJoint = gr.joint('neckJoint', {0, 0, 0}, {135, 180, 225})
+leftUpperArmJoint = gr.joint('leftUpperArmJoint', {0, 0, 0}, {0, 0, 90})
+leftUpperArmJointMove:add_child(leftUpperArmJoint)
+
+leftUpperArm = gr.node('leftUpperArm')
+leftUpperArmJoint:add_child(leftUpperArm)
+leftUpperArm:translate(0.0, -0.3, 0)
+
+leftUpperArmScale = gr.mesh('sphere', 'leftUpperArmScale')
+leftUpperArm:add_child(leftUpperArmScale)
+leftUpperArmScale:scale(0.13, 0.32, 0.13)
+leftUpperArmScale:set_material(blue)
+
+leftForearmJointMove = gr.node('leftForearmJointMove')
+leftUpperArm:add_child(leftForearmJointMove)
+leftForearmJointMove:translate(0.0, -0.3, 0.0)
+
+leftForearmJoint = gr.joint('leftForearmJoint', {0, 0, 0}, {0, 0, 90})
+leftForearmJointMove:add_child(leftForearmJoint)
+
+leftForearm = gr.node('leftForearm')
+leftForearmJoint:add_child(leftForearm)
+leftForearm:translate(0.0, -0.29, 0.0)
+
+leftForearmScale = gr.mesh('sphere', 'leftForearmScale')
+leftForearm:add_child(leftForearmScale)
+leftForearmScale:scale(0.07, 0.27, 0.07)
+leftForearmScale:set_material(blue)
+
+leftHandJointMove = gr.node('leftHandJointMove')
+leftForearm:add_child(leftHandJointMove)
+leftHandJointMove:translate(0.0, -0.29, 0.0)
+
+leftHandJoint = gr.joint('leftHandJoint', {0, 0, 0}, {0, 0, 90})
+leftHandJointMove:add_child(leftHandJoint)
+
+leftHand = gr.node('leftHand')
+leftHandJoint:add_child(leftHand)
+leftHand:translate(0.0, -0.02, 0.0)
+
+leftHandScale = gr.mesh('sphere', 'leftHandScale')
+leftHand:add_child(leftHandScale)
+leftHandScale:scale(0.05, 0.05, 0.05)
+leftHandScale:set_material(blue)
+
+leftThighJointMove = gr.node('leftThighJointMove')
+hip:add_child(leftThighJointMove)
+leftThighJointMove:translate(0.20, -0.03, 0.0)
+
+leftThighJoint = gr.joint('leftThighJoint', {0, 0, 0}, {0, 0 , 45})
+leftThighJointMove:add_child(leftThighJoint)
+
+leftThigh = gr.node('leftThigh')
+leftThighJoint:add_child(leftThigh)
+leftThigh:translate(0.0, -0.40, 0.0)
+
+leftThighScale = gr.mesh('sphere', 'leftThighScale')
+leftThigh:add_child(leftThighScale)
+leftThighScale:scale(0.13, 0.40, 0.13)
+leftThighScale:set_material(blue)
+
+leftCalfJointMove = gr.node('leftCalfJointMove')
+leftThigh:add_child(leftCalfJointMove)
+leftCalfJointMove:translate(0.0, -0.40, 0.0)
+
+leftCalfJoint = gr.joint('leftCalfJoint', {0, 0, 0}, {-45, 0 , 45})
+leftCalfJointMove:add_child(leftCalfJoint)
+
+leftCalf = gr.node('leftCalf')
+leftCalfJoint:add_child(leftCalf)
+leftCalf:translate(0.0, -0.34, 0.0)
+
+leftCalfScale = gr.mesh('sphere', 'leftCalfScale')
+leftCalf:add_child(leftCalfScale)
+leftCalfScale:scale(0.13, 0.34, 0.13)
+leftCalfScale:set_material(blue)
+
+leftFootJointMove = gr.node('leftFootJointMove')
+leftCalf:add_child(leftFootJointMove)
+leftFootJointMove:translate(0.0, -0.34, 0.0)
+
+leftFootJoint = gr.joint('leftFootJoint', {0, 0, 0}, {90, 90, 135})
+leftFootJointMove:add_child(leftFootJoint)
+
+leftFoot = gr.node('leftFoot')
+leftFootJoint:add_child(leftFoot)
+leftFoot:translate(0.0, -0.20, 0.0)
+
+leftFootScale = gr.mesh('sphere', 'leftFootScale')
+leftFoot:add_child(leftFootScale)
+leftFootScale:scale(0.05, 0.20, 0.05)
+leftFootScale:set_material(green)
+
+headRotateMove = gr.node('headRotateMove')
+shoulder:add_child(headRotateMove)
+headRotateMove:translate(0.0, 0.10, 0.0)
+
+headRotate = gr.joint('headRotate', {-90, 0, 90}, {0, 0, 0});
+headRotateMove:add_child(headRotate)
+
+neckJointMove = gr.node('neckJointMove')
+headRotate:add_child(neckJointMove)
+
+neckJoint = gr.joint('neckJoint', {0, 0, 0}, {-30, 0, 30});
 neckJointMove:add_child(neckJoint)
 
 neck = gr.node('neck')
 neckJoint:add_child(neck)
-neck:translate(0.0, 0.45, 0.0)
+neck:translate(0.0, 0.15, 0.0)
 
 neckScale = gr.mesh('sphere', 'neckScale')
 neck:add_child(neckScale)
-neckScale:scale(0.10, 0.15, 0.10) 
+neckScale:scale(0.10, 0.15, 0.10)
+neckScale:set_material(blue)
+
+headJointMove = gr.node('headJointMove')
+neck:add_child(headJointMove)
+headJointMove:translate(0.0, 0.07, 0.0)
+
+headJoint = gr.joint('headJoint', {0, 0, 0}, {-15, 0, 15})
+headJointMove:add_child(headJoint)
+
+head = gr.node('head')
+headJoint:add_child(head)
+head:translate(0.0, 0.3, 0.0)
+
+headScale = gr.mesh('sphere', 'headScale')
+head:add_child(headScale)
+headScale:scale(0.3, 0.3, 0.3)
+headScale:set_material(green)
+
+leftEye = gr.mesh('sphere', 'leftEye')
+head:add_child(leftEye)
+leftEye:scale(0.05, 0.05, 0.05)
+leftEye:translate(0.1, 0.1, 0.0)
+leftEye:set_material(red)
+
+rightEye = gr.mesh('sphere', 'rightEye')
+head:add_child(rightEye)
+rightEye:scale(0.05, 0.05, 0.05)
+rightEye:translate(-0.1, 0.1, 0.0) 
+rightEye:set_material(red)
 
 return rootnode
