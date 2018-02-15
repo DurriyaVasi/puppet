92546c96e81fbac0e6bf7b9ecf90fb4b82b9c73e
Durriya Vasi <dvasi@uwaterloo.ca>
2018-02-14 23:07:58 -0500

    added pupprt eith joinst correctly

---
+++ Assets/puppet2.lua
@@ -0,0 +1,91 @@
+-- puppet.lua
+-- A simplified puppet without posable joints, but that
+-- looks roughly humanoid.
+
+rootnode = gr.node('root')
+rootnode:scale( 0.25, 0.25, 0.25 )
+rootnode:translate(0.0, 0.0, -1.0)
+
+rootRotateNode = gr.node('rootRotate')
+rootnode:add_child(rootRotateNode)
+
+red = gr.material({1.0, 0.0, 0.0}, {0.1, 0.1, 0.1}, 10)
+blue = gr.material({0.0, 0.0, 1.0}, {0.1, 0.1, 0.1}, 10)
+green = gr.material({0.0, 1.0, 0.0}, {0.1, 0.1, 0.1}, 10)
+white = gr.material({1.0, 1.0, 1.0}, {0.1, 0.1, 0.1}, 10)
+
+torso = gr.node('torso')
+rootRotateNode:add_child(torso)
+
+torsoScale = gr.mesh('sphere', 'torso')
+torso:add_child(torsoScale)
+torsoScale:scale(0.5, 0.8, 0.5)
+torsoScale:scale(0.7, 0.7, 0.7)
+torsoScale:set_material(white)
+
+shoulder = gr.node('shoulder')
+torso:add_child(shoulder)
+shoulder:translate(0.0, 0.5, 0.0)
+
+shoulderScale = gr.mesh('sphere', 'shoulderScale')
+shoulder:add_child(shoulderScale)
+shoulderScale:scale(0.6, 0.13, 0.13)
+shoulderScale:set_material(red)
+
+rightUpperArmJointMove = gr.node('rightUpperArmJointMove')
+shoulder:add_child(rightUpperArmJointMove)
+rightUpperArmJointMove :translate(-0.55, 0.0, 0.0)
+
+rightUpperArmJoint = gr.joint('rightUpperArmJoint', {0, 0, 0}, {-90, 0, 0})
+rightUpperArmJointMove:add_child(rightUpperArmJoint)
+
+rightUpperArm = gr.node('rightUpperArm')
+rightUpperArmJoint:add_child(rightUpperArm)
+rightUpperArm:translate(0.0, -0.3, 0)
+
+rightUpperArmScale = gr.mesh('sphere', 'rightUpperArmScale')
+rightUpperArm:add_child(rightUpperArmScale)
+rightUpperArmScale:scale(0.13, 0.32, 0.13)
+rightUpperArmScale:set_material(blue)
+
+rightForearmJointMove = gr.node('rightForearmJointMove')
+rightUpperArm:add_child(rightForearmJointMove)
+rightForearmJointMove:translate(0.0, -0.3, 0.0)
+
+rightForearmJoint = gr.joint('rightForearmJoint', {0, 0, 0}, {-90, 0, 0})
+rightForearmJointMove:add_child(rightForearmJoint)
+
+rightForearm = gr.node('rightForearm')
+rightForearmJoint:add_child(rightForearm)
+rightForearm:translate(0.0, -0.29, 0.0)
+
+rightForearmScale = gr.mesh('sphere', 'rightForearmScale')
+rightForearm:add_child(rightForearmScale)
+rightForearmScale:scale(0.07, 0.27, 0.07)
+rightForearmScale:set_material(blue)
+
+rightHandJointMove = gr.node('rightHandJointMove')
+rightForearm:add_child(rightHandJointMove)
+rightHandJointMove:translate(0.0, -0.29, 0.0)
+
+rightHandJoint = gr.joint('rightHandJoint', {0, 0, 0}, {-90, 0, 0})
+rightHandJointMove:add_child(rightHandJoint)
+
+rightHand = gr.node('rightHand')
+rightHandJoint:add_child(rightHand)
+rightHand:translate(0.0, -0.02, 0.0)
+
+rightHandScale = gr.mesh('sphere', 'rightHandScale')
+rightHand:add_child(rightHandScale)
+rightHandScale:scale(0.05, 0.05, 0.05)
+rightHandScale:set_material(blue)
+
+hip = gr.node('hip')
+torso:add_child(hip)
+hip:translate(0.0, -0.55, 0.0)
+
+hipScale = gr.mesh('sphere', 'hipScale')
+hip:add_child(hipScale)
+hipScale:scale(0.35, 0.15, 0.15) 
+
+return rootnode
